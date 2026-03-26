0845c6cdb2bab10bf32a16b264394d60e7358a00
Danylo Mikhnytskyi <@danilmickhnitskyy>
2026-03-26 23:06:08 +0100

    Clean code

--- features/pageobjects/secure.page.js
+++ features/pageobjects/secure.page.js
@@ -1,16 +1,9 @@
-const { $ } = require('@wdio/globals')
-const Page = require('./page');
-
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+const { $ } = require("@wdio/globals");
+const Page = require("./page");
 class SecurePage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    get flashAlert () {
-        return $('#flash');
-    }
+  get flashAlert() {
+    return $("#flash");
+  }
 }
 
 module.exports = new SecurePage();
